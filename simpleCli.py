09e7b5e78b5fcc998ae1ce818a115a62f33bf75f
Kevin Renner <krenner529@gmail.com>
2017-03-06 23:22:54 -0600

    added aes encryption of message with rsa encrypted key exchange

--- simpleCli.py
+++ simpleCli.py
@@ -1,12 +1,24 @@
 from Crypto.PublicKey import RSA
+from Crypto.Cipher import AES
+from Crypto import Random
 import socket
 
+randfile = Random.new()
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('127.0.0.1', 6066))
 publickey = s.recv(500)
 key = RSA.importKey(publickey)
-plaintext = "carl is bad"
-ciphertext = key.encrypt(plaintext, key.publickey())
-s.send(ciphertext[0])
-print "client sent ciphertext"
+message = "carl is bad"
+# Pad message
+pad_size = 128 - len(message) % 128
+if pad_size == 0:
+    pad_size = 128
+message += "0" * pad_size
+aes_key = randfile.read(32)
+aes_obj = AES.new(aes_key, AES.MODE_CBC, "0"*16)
+ciphertext_rsa = key.encrypt(aes_key, key.publickey())
+ciphertext_aes = aes_obj.encrypt(message)
+s.send(ciphertext_rsa[0])
+s.send(ciphertext_aes)
+print "client sent aes key"
 s.close()
--- simpleSrv.py
+++ simpleSrv.py
@@ -1,4 +1,5 @@
 from Crypto.PublicKey import RSA
+from Crypto.Cipher import AES
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -9,7 +10,11 @@ s.listen(1)
 key = RSA.generate(2048)
 publickey = key.exportKey(format = "OpenSSH", passphrase=None, pkcs = 1)
 clientsocket.send(publickey)
-ciphertext = clientsocket.recv(500)
-plaintext = key.decrypt(ciphertext)
+ciphertext_rsa = clientsocket.recv(500)
+ciphertext_aes = clientsocket.recv(1024)
+aes_key = key.decrypt(ciphertext_rsa)
+aes_obj = AES.new(aes_key, AES.MODE_CBC, "0"*16)
+print len(ciphertext_aes)
+plaintext = aes_obj.decrypt(ciphertext_aes)
 print "server got: " + plaintext + "\n"
 clientsocket.close()
