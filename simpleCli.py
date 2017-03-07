a83bfaf4b27cb754a767d2ac9ccfad5f40df7baa
Kevin Renner <krenner529@gmail.com>
2017-03-07 10:46:39 -0600

    added message signing

--- simpleCli.py
+++ simpleCli.py
@@ -1,11 +1,15 @@
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import AES
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.Hash import SHA256
 from Crypto import Random
+from base64 import b64encode, b64decode
 import socket
 
 randfile = Random.new()
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('127.0.0.1', 6066))
+# Get servers public key
 publickey = s.recv(500)
 key = RSA.importKey(publickey)
 message = "carl is bad"
@@ -14,11 +18,24 @@ pad_size = 128 - len(message) % 128
 if pad_size == 0:
     pad_size = 128
 message += "0" * pad_size
+# Sign message
+keyfile = open('private.pem', 'r').read()
+cl_priv_key = RSA.importKey(keyfile)
+signer = PKCS1_v1_5.new(cl_priv_key)
+digest = SHA256.new()
+digest.update(message)
+sign = signer.sign(digest)
+signature = b64encode(sign)
+# Initialize AES
 aes_key = randfile.read(32)
 aes_obj = AES.new(aes_key, AES.MODE_CBC, "0"*16)
 ciphertext_rsa = key.encrypt(aes_key, key.publickey())
 ciphertext_aes = aes_obj.encrypt(message)
+print len(ciphertext_aes)
+print len(signature)
+# Send messages
 s.send(ciphertext_rsa[0])
 s.send(ciphertext_aes)
-print "client sent aes key"
+s.send(signature)
+print "client sent messages"
 s.close()
--- simpleSrv.py
+++ simpleSrv.py
@@ -1,5 +1,8 @@
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import AES
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.Hash import SHA256
+from base64 import b64decode, b64encode
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -7,14 +10,24 @@ s.bind(('127.0.0.1', 6066))
 s.listen(1)
 
 (clientsocket, addr) = s.accept()
+# Generate and send public key to client
 key = RSA.generate(2048)
 publickey = key.exportKey(format = "OpenSSH", passphrase=None, pkcs = 1)
 clientsocket.send(publickey)
+# Receives messages (using fixed size buffers until protocol is determined)
 ciphertext_rsa = clientsocket.recv(500)
-ciphertext_aes = clientsocket.recv(1024)
+ciphertext_aes = clientsocket.recv(128)
+signature = clientsocket.recv(344)
+# Decrypt key and message
 aes_key = key.decrypt(ciphertext_rsa)
 aes_obj = AES.new(aes_key, AES.MODE_CBC, "0"*16)
-print len(ciphertext_aes)
 plaintext = aes_obj.decrypt(ciphertext_aes)
-print "server got: " + plaintext + "\n"
+# Verify messages
+cli_pub_keyfile = open("public.pem", "r").read()
+cli_pub_key = RSA.importKey(cli_pub_keyfile)
+signer = PKCS1_v1_5.new(cli_pub_key)
+digest = SHA256.new()
+digest.update(plaintext)
+verified = signer.verify(digest, b64decode(signature))
+print "server got: " + plaintext + "\nVerified: " + str(verified)
 clientsocket.close()
