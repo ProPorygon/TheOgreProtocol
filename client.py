3e51821b2589164e3b7177e0092bcf6f99542d5c
Kevin Renner <krenner529@gmail.com>
2017-04-25 16:12:49 -0500

    brought client up to date

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import AES
+from Crypto import Random
 import socket
 import utils
 
@@ -8,39 +9,43 @@ DA_PORT = 4444
 DA_IP = "127.0.0.1"
 
 # Stuff with the directory authority, need to work out how this works
+# da_pub_key =
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((DA_IP, DA_PORT))
-rsa_key = RSA.generate(1024)
-publickey = rsa_key.exportKey(format="OpenSSH", passphrase=None, pkcs=1)
-s.send(publickey)
+utils.send_message_with_length_prefix(s, 'r')
 # Receive
-aes_key_string = s.recv(128)
-aes_key = AES.new(aes_key_string, AES.MODE_CBC, "0"*16)
-s.recv()  # How much should this receive? All info from directory authority
+# aes_key_string = utils.recv_message_with_length_prefix(s)
+# aes_key = AES.new(aes_key_string, AES.MODE_CBC, "0"*16)
+data = utils.recv_message_with_length_prefix(s)  # All info from directory authority
+hop_data = utils.unwrap_message(data, da_pub_key)
 
 # Process received route data
 
+# hoplist format (ip, port, public_key)
 hoplist = []  # Replace this with processed route and key data
 # Send keys and establish link
-next_aes_key = AES.new(hoplist[0][1], AES.MODE_CBC, "0"*16)
 next_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-next_host = utils.unpackHostPort(hoplist[0][0])
+next_host = (hoplist[0][0], hoplist[0][1])
 next_s.connect(next_host)
 wrapped_message = ""
+randfile = Random.new()
+aes_key_list = []
 # Generate wrapped message
 for elem in hoplist:
-    elem_rsa_pubkey  # have some way of getting each, probably from directory authority
-    wrapped_message = utils.wrap_message(wrapped_message, elem_rsa_pubkey, elem[1])
+    # have some way of getting each, probably from directory authority
+    elem_aes_key = randfile.read(32)
+    aes_key_list.append(elem_aes_key)
+    wrapped_message = utils.wrap_message(wrapped_message, elem[2], elem_aes_key)
 utils.send_message_with_length_prefix(next_s, wrapped_message)
 
 while(True):
     message = raw_input()
-    for elem in hoplist:
-        message = utils.add_layer(message, elem[1])
+    for key in aes_key_list:
+        message = utils.add_layer(message, key)
     next_s.send(message)
     utils.send_message_with_length_prefix(next_s, message)
-    recieved_message = utils.recv_message_with_length_prefix(next_s)
+    response = utils.recv_message_with_length_prefix(next_s)
     response = ""
-    for i in reversed(range(0, len(hoplist))):
-        response = utils.peel_layer(message, hoplist[i][1])
+    for i in reversed(range(0, len(aes_key_list))):
+        response = utils.peel_layer(response, aes_key_list[i])
     print response
